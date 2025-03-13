1eb4799402eeec3bdc51414588eb9ce7cae4c0de
Fondation Jean Dausset - CEPH <21590769+FJD-CEPH@users.noreply.github.com>
2025-03-13 13:10:43 +0100

    Update MSIExpresso.pl
    Up to date including MMR Status score revised

--- MSIExpresso.pl
+++ MSIExpresso.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# MSI Search Tools V0.8 2018
+# MSI Search Tools V2.0 2025
 # Emmanuel Tubacher
 # Fondation Jean Dausset-CEPH
 # Read the configuration file and store all parameters in hash table and in variables for easier access
@@ -7,9 +7,20 @@ my %parameters;
 readConfigFile();
 checkParameters();
 my $outpath="$parameters{'output dir'}/$parameters{'output files root'}";
-print STDERR "Using output directory $outpath for all files\n";
+print STDERR "Using output directory $parameters{'output dir'}, output all files names starting with $parameters{'output files root'}\n";
 if ($parameters{'output files root'} ne "") {
 	$outpath=$outpath.".";
+}
+
+if ($parameters{'MMR status'} eq 'y' || $parameters{'MMR status'} eq 'yes') {
+	MMRStatus();
+}
+if ($ARGV[1] eq "panelOnly") {
+	my $panelDB=makePanelDB();
+	%MSIintronicMicrosatellites=searchCodingMicrosats($outpath,$samout,$readfiles);	
+	rmPanelDB();
+	rerunMSIStatus();
+	exit(0);
 }
 if ($ARGV[1] eq "updateStatus") {
 	rerunMSIStatus();
@@ -36,16 +47,14 @@ if ($parameters{'skipping'} eq "yes") {
 	searchExonSkipping($outpath,$samout,$readfiles,%MSIintronicMicrosatellites);
 }
 
-#if ($parameters{'check NMD'} eq "yes") {
-#	searchNMD($outpath,$samout,$readfiles);
-#}
-
 sub rerunMSIStatus {
 	reassignStatus("CODING");
 	my $codingFile="$parameters{'output dir'}/$parameters{'output files root'}.CODING.counts.csv";
 	getMsiStatus($codingFile);
-	reassignStatus("INTRONIC");
-	reassignSkippingStatus();
+	if ($ARGV[1] ne 'panelOnly') {
+		reassignStatus("INTRONIC");
+		reassignSkippingStatus();
+	}
 }
 
 sub reassignStatus {
@@ -98,7 +107,7 @@ sub getStatusScore {
 sub reassignSkippingStatus {
 	my $skippingFile="$parameters{'output dir'}/$parameters{'output files root'}.SKIPPING.counts.csv";
 	my $intronicFile="$parameters{'output dir'}/$parameters{'output files root'}.INTRONIC.counts.csv";
-	open(INTRONIC,"cut -f1,5,6 $intronicFile|") || die "Can't open $inotrnicFile\n";
+	open(INTRONIC,"cut -f1,5,6 $intronicFile|") || die "Can't open $intronicFile\n";
 	my $hd=<INTRONIC>;
 	my %intronStatus;
 	my %intronScore;
@@ -191,6 +200,12 @@ sub searchCodingMicrosats {
 	my $bwaNumThreads=$parameters{'bwa threads'};
 	if ($bwaNumThreads ne "") { $bwaNumThreads=" -t $bwaNumThreads";}
 	my $bwaCodingCmd="$parameters{'path to bwa'} mem $bwaNumThreads $parameters{'database dir'}/CODING.fa $parameters{'fastq read1'}  $parameters{'fastq read2'} | $parameters{'path to samtools'} view -S -F4 -q 30 - ";
+if ($ARGV[1] eq "panelOnly") {
+                print STDOUT "Computing MSI Status only on $parameters{'output dir'}/$parameters{'panel DB'}\n";
+                $bwaCodingCmd="$parameters{'path to bwa'} mem $bwaNumThreads $parameters{'output dir'}/$parameters{'panel DB'}  $parameters{'fastq read1'} $parameters{'fastq read2'} | $parameters{'path to samtools'} view -S -F4 -q 30 -";
+print STDOUT "$bwaCodingCmd";
+        }
+
 	open(IN,"$bwaCodingCmd|") ||die "Command $bwaCodingCmd failed\n";
 	while (my $line = <IN>) {
 		chomp $line;
@@ -641,10 +656,26 @@ sub getTypeFromCigar {
 }
 
 sub readConfigFile {
-	print STDERR "Reading configuration file\n";
-	if ($ARGV[0] eq "") {die "Usage : ./MSIExpresso.pl <configuration file>\n";}
+	print STDERR "Reading sample configuration file\n";
+	if ($ARGV[0] eq "") {die "Usage : ./MSInspector.pl <sample configuration file>\n";}
+	open(IN,"grep 'global configuration' $ARGV[0]| cut -f2 -d'='|");
+	my $line=<IN>;
+	if ($line ne '') {
+		chomp $line;
+		print STDERR "Reading global configuration file: $line\n";
+		open(GLOBAL,"grep -v '#' $line|") ||die "No global configuration file $line!\n";
+		while (my $line=<GLOBAL>) {
+			chomp $line;
+			my ($key,$value)=split('=',$line);
+                        $parameters{$key}=$value;
+			print STDERR "$key=$value\n";
+                }
+                close GLOBAL;			
+	}	
+	close IN;
         open(IN,"grep -v '#' $ARGV[0]|") ||die "No configuration file $ARGV[0]!\n";
-        while (my $line = <IN>) {
+	print STDERR "Reading sample configuration file $ARGV[0]!\nDuplicates entries from global configuration file will be overriden\n"; 
+       while (my $line = <IN>) {
                 chomp $line;
                 if ($line =~ "#" || $line eq "") { next;}
                 my ($key,$value)=split(/\=/,$line);
@@ -654,39 +685,319 @@ sub readConfigFile {
         close IN;
 }
 sub checkParameters {
-if ($ARGV[1] ne "updateStatus") {
-	if (! -f $parameters{'fastq read1'}) {
-		die "$parameters{'fastq read1'} fastq file not found\n";
-	}
-	if ($parameters{'fastq read2'} ne "") {
-		if (! -f $parameters{'fastq read2'}) {
-			die "$parameters{'fastq read2'} fastq file not found\n";
+	if ($ARGV[1] ne "updateStatus") {
+		if (! -f $parameters{'fastq read1'}) {
+			die "$parameters{'fastq read1'} fastq file not found\n";
+		}
+		if ($parameters{'fastq read2'} ne "") {
+			if (! -f $parameters{'fastq read2'}) {
+				die "$parameters{'fastq read2'} fastq file not found\n";
+			}
+		}
+		if ($parameters{'minimum overlap length'} eq "") {
+			$parameters{'minimum overlap length'}=10;
+			print STDERR "minimum overlap length parameter not set, default value of 10bp will be used\n";
 		}
 	}
-	if ($parameters{'minimum overlap length'} eq "") {
-		$parameters{'minimum overlap length'}=10;
-		print STDERR "minimum overlap length parameter no set, default value of 10bp will be used\n";
+	open(BWA,"which $parameters{'path to bwa'}|") ||die ("bwa not found, please set and check path to bwa in configuration file\n");
+	my $bwa=<BWA>;
+	chomp $bwa;
+	if ($bwa eq'') {
+		die "bwa not found, please set and check path to bwa in configuration file\n";
 	}
-}
+	close BWA;
+	open(SAM,"which $parameters{'path to samtools'}|") ||die "samtools not found, please set and check path to samtools in configuration file\n";
+	my $sam=<SAM>;
+	chomp $sam;
+	if ($sam eq'') {
+		die "samtools not found, please set and check path to samtools in configuration file\n";
+	}
+	close SAM;
+	open(BCF,"which $parameters{'path to bcftools'}|") ||die "bcftools not found, please set and check path to bcftools in configuration file\n";
+	my $bcf=<BCF>;
+	chomp $bcf;
+	if ($bcf eq'') {
+		die "bcftools not found, please set and check path to bcftools in configuration file\n";
+	}
 	if ($parameters{"MSI-H cutoff"} eq "") {
 		$parameters{"MSI-H cutoff"}=30;
-		print STDERR "MSI-H cutoff parameter no set, default value of 30% will be used\n";
+		print STDERR "MSI-H cutoff parameter not set, default value of 30% will be used\n";
 	}
 	if ($parameters{"MSI-L cutoff"} eq "") {
 		$parameters{"MSI-H cutoff"}=10;
-		print STDERR "MSI-L cutoff parameter no set, default value of 10% will be used\n";
+		print STDERR "MSI-L cutoff parameter not set, default value of 10% will be used\n";
 	}
 	if ($parameters{"minimal expressed UTR"} eq "") {
 		$parameters{"minimal expressed UTR"}=30;
-		print STDERR "minimal expressed UTR parameter no set, default value of 30 will be used\n";
+		print STDERR "minimal expressed UTR parameter not set, default value of 30 will be used\n";
 	}
 	if ($parameters{"minimum coverage"} eq "") {
 		$parameters{"minimum coverage"}=15;
-		print STDERR "minimum coverage parameter no set, default value of 15 will be used\n";
+		print STDERR "minimum coverage parameter not set, default value of 15 will be used\n";
 	}
 	if ($parameters{"database sequence length"} eq "") {
 		$parameters{"database sequence length"}=60;
 	}
 }
 
+sub makePanelDB {
+	%panelMS;
+	open(PN,$parameters{"UTR panel"}) ||die "UTR Panel $parameters{'UTR panel'} not found\n";;
+	my $dbname="paneldb$$.fa";
+	while (my $line=<PN>) {
+		chomp $line;
+		my @pn=split(/\t/,$line);
+		$panelMS{">$pn[0]"}=1;
+	}
+	close PN;
+	my $MSCount=0;
+	open(DB,"$parameters{'database dir'}/CODING.fa") || die;
+	open(DBPANEL,"> $parameters{'output dir'}/$dbname") || die;
+	while (my $line=<DB>) {
+		chomp $line;
+		my @data=split(/#/,$line);
+		if ($panelMS{$data[0]}==1) {
+			print DBPANEL "$line\n";
+			my $seq=<DB>;
+			print DBPANEL $seq;
+			$panelMS{$data[0]}=0;
+			$MSCount++;
+		}
+	}
+	close DB;
+	if ($MSCount < $#panelMS) {
+		open(DB,"$parameters{'database dir'}/INTRONIC.fa") || die;
+		while (my $line=<DB>) {
+			chomp $line;
+			my @data=split(/#/,$line);
+			if ($panelMS{$data[0]}==1) {
+				print DBPANEL "$line\n";
+				my $seq=<DB>;
+				$panelMS{$data[0]}=0;
+				$MSCount++;
+			}
+		}
+	}
+	close DBPANEL;
+	if ($MSCount < $#panelMS) {
+		foreach my $id (sort keys %panelMS) {
+			if ($panelMS{$id}==1) {print STDERR "$id not in INTRONIC.fa nor CODING.fa sequences databases\n";}
+		}
+	}
+	system("$parameters{'path to bwa'} index $parameters{'output dir'}/$dbname");
+	print STDERR "PANEL database $parameters{'output dir'}/$dbname created and bwa indexed\n";
+	$parameters{'panel DB'}=$dbname;
+	return $dbname;
+}
+sub rmPanelDB {
+	unlink($arameters{"output dir"}."/".$parameters{'panel DB'});
+}
+sub MMRStatus {
+	my %cdsLength;
+	open(CDS, "$parameters{'database dir'}/MMR.cds.fa") ||die;
+	        my @lines;
+        my @heads;
+        my @gns;
+        my $nline=0;
+        while (my $line=<CDS>) {
+                chomp $line;
+                if ($line =~'>') {
+                        $line=~s/\>//;
+                        push (@gns,$line);
+                        push (@heads,$nline);
+                }
+                push (@lines,$line);
+                $nline++;
+
+        }
+        push(@heads,$nline);
+        close CDS;
+        for (my $i=0; $i<= $#gns; $i++) {
+                my $seq='';
+                for (my $j=$heads[$i]+1; $j < $heads[$i+1]; $j++) {
+                        $seq=$seq.$lines[$j];
+                }
+                $cdsLength{$gns[$i]}=length($seq);
+        }
+	my $outBam="$parameters{'output dir'}/$parameters{'output files root'}_MMR.bam";
+	my $outVCF="$parameters{'output dir'}/$parameters{'output files root'}_MMR.vcf";
+	print STDERR "Writing $outBam and $outVCF\n";
+	my $mmrCommand="$parameters{'path to bwa'} mem $bwaNumThreads $parameters{'database dir'}/MMR.cds.fa $parameters{'fastq read1'}  $parameters{'fastq read2'} |$parameters{'path to samtools'} view -S -h -F4 -q 30 - |$parameters{'path to samtools'} sort -o $outBam ";
+	system($mmrCommand);
+	my $mmrCommand2="$parameters{'path to samtools'} view $outBam |cut -f3|sort |uniq -c|awk '{print \$2\" \"\$1}'";
+	open(COUNTS,"$mmrCommand2|") ||die;
+	my $m6=0;
+	my $m2=0;
+	my $m1=0;
+	my $tot=0;
+	my $p6=0;
+	my $p2=0;
+	my $p1=0;
+	while (my $line=<COUNTS>) {
+		chomp $line;
+        	my @m=split(/\s/,$line);
+        	if ($m[0] eq 'MSH6') {
+			$m6=$m[1]; }
+        	if ($m[0] eq 'MSH2') {
+        		$m2=$m[1];
+		}
+        	if ($m[0] eq 'MLH1') {
+       			$m1=$m[1];
+		}
+	}
+	close COUNTS;
+	$m6=$m6*1000/$cdsLength{'MSH6'};
+	$m2=$m2*1000/$cdsLength{'MSH2'};
+	$m1=$m1*1000/$cdsLength{'MLH1'};
+	$tot=$m1+$m2+$m6;
+        $p6=100*$m6/$tot;
+        $p2=100*$m2/$tot;
+        $p1=100*$m1/$tot;
+	open(STATS,"> $parameters{'output dir'}/$parameters{'output files root'}_MMRStatus.txt")||die "Open $parameters{'output dir'}/$parameters{'output files root'}_MMRStatus.txt\n";
+        if ($tot < 300) {print STAT "Not enough reads to estimate MMR deficiency accurately !\n";}
+	print STATS "Relative expression of MLH1, MSH2 and MSH6\n";
+	print STATS "MLH1\t$p1\t";
+	my $st=1;
+	if ($p1 <16) {print STATS "MLH1 low expression"; $st=0;} else { print STATS "OK";}
+	print STATS "\nMSH2\t$p2\t";
+	if ($p2 <16) {print STATS "MSH2 low expression"; $st=0;} else { print STATS "OK";}
+	print STATS "\nMSH6\t$p6\t";
+	if ($p6 <11) {print STATS "MSH6 low expression"; $st=0;} else { print STATS "OK";}
+	print STATS "\n";
+	if ($st==1) {print STATS "MMR genes normal expression\n";} else { print STATS "MMR genes loss of expression\n";}
+	my $variantCmd="$parameters{'path to bcftools'} mpileup -f $parameters{'database dir'}/MMR.cds.fa $outBam | bcftools call -mv -Ov -o $outVCF";
+	system($variantCmd);
+	my @variants=searchFunctionalVariants($outVCF);	 
+	if ($#variants <0) {
+		print STATS "No functional variant found in MMR genes\n";
+	}
+	else {
+		print STATS "List of functional variants in MMR genes\nGene name\tCDS position\tVaraitn description\n";
+		foreach my $v (@vars) {
+			$v=~s/#/\t/g;
+			print STATS "$v\n";
+		}
+	}
+	close STATS;
+	print STDOUT "MMR status available: $parameters{'output dir'}/$parameters{'output files root'}_MMRStatus.txt\n";
+}
+sub searchFunctionalVariants {
+	my ($vcf)=@_;
+	my %gseq;
+	open(SEQ,"$parameters{'database dir'}/MMR.cds.fa") ||die "No MMMR database found\n";
+	my $sq='';
+	my $gname='';	
+	while (my $line=<SEQ>) {
+		chomp $line;
+		if ($line =~'>') {
+			if ($gname ne '' && $sq ne '') {
+				$gseq{$gname}=$sq;
+				$gname=$line;
+				$gname=~s/>//g;
+				$sq='';
+			}
+		}
+		else {
+			$sq=$sq.$line;
+		}
+	}
+	close SEQ;
+	$gseq{$gname}=$sq;
+	my (%codons_table) = ( 'TTT' => 'F', 'TTC' => 'F', 'TTA' => 'L', 'TTG' => 'L', 'CTT' => 'L', 'CTC' => 'L', 'CTA' => 'L', 'CTG' => 'L',
+    'ATT' => 'I', 'ATC' => 'I', 'ATA' => 'I', 'ATG' => 'M', 'GTT' => 'V', 'GTC' => 'V', 'GTA' => 'V', 'GTG' => 'V', 'TCT' => 'S', 'TCC' => 'S',
+    'TCA' => 'S', 'TCG' => 'S', 'CCT' => 'P', 'CCC' => 'P', 'CCA' => 'P', 'CCG' => 'P', 'ACT' => 'T', 'ACC' => 'T', 'ACA' => 'T', 'ACG' => 'T',
+    'GCT' => 'A', 'GCC' => 'A', 'GCA' => 'A', 'GCG' => 'A', 'TAT' => 'Y', 'TAC' => 'Y', 'TAA' => '.', 'TAG' => '.', 'CAT' => 'H', 'CAC' => 'H',
+    'CAA' => 'Q', 'CAG' => 'Q', 'AAT' => 'N', 'AAC' => 'N', 'AAA' => 'K', 'AAG' => 'K', 'GAT' => 'D', 'GAC' => 'D', 'GAA' => 'E', 'GAG' => 'E',
+    'TGT' => 'C', 'TGC' => 'C', 'TGA' => '.', 'TGG' => 'W', 'CGT' => 'R', 'CGC' => 'R', 'CGA' => 'R', 'CGG' => 'R', 'AGT' => 'S', 'AGC' => 'S',
+    'AGA' => 'R', 'AGG' => 'R', 'GGT' => 'G', 'GGC' => 'G', 'GGA' => 'G', 'GGG' => 'G',);
+	my @vars;
+	open(VCF,"cut -f1,2,4,5,8 $vcf|grep -v '#'|") || die;
+	while (my $line=<VCF>) {
+        	chomp $line;
+        	my ($gene,$pos,$all1,$all2,$dp)=split(/\t/,$line);
+       		my @vals=split(';',$dp);
+        	my $nba1=0;
+       		my $nba2=0;
+        	foreach my $val (@vals) {
+                	if ($val =~'DP4') {	
+				$val=~s/DP4=//;
+                        	my ($f1,$r1,$f2,$r2)=split(',',$val);
+                        	$nba1=$f1+$r1;
+                        	$nba2=$f2+$r2;
+                        	if ($nba2 > $nba1) {
+                                	my $ta=$all1;
+                                	$all1=$all2;
+                                	$all2=$ta;
+                        	}
+                	}
+        	}
+		if ($nba1+$nba2 < 20) {next;}
+        	my $gnseq="";
+        	my $utr3="";
+        	my $cds="";
+        	my $utr5="";
+                $gnseq=$gseq{$gene};
+        	($utr5,$cdutr)=split('atg',$gnseq);
+        	$cdutr='ATG'.$cdutr;
+        	if ($gnseq =~ 'taa') {
+                	($cds,$utr3)=split('taa',$cdutr);
+                	$cds=$cds.'TAA';
+        	}
+        	if ($gnseq =~ 'tag') {
+                	($cds,$utr3)=split('tag',$cdutr);
+                	$cds=$cds.'TAG';
+        	}
+        	if ($gnseq =~ 'tga') {
+                	($cds,$utr3)=split('tga',$cdutr);
+                	$cds=$cds.'TGA';
+        	}
+        	my $u5l=length($utr5);
+        	my $u3l=length($utr3);
+        	$cds=$cds.uc(substr($gnseq,3,$u5l+3+length($cds)));
+        	my $cdl=length($cds);
+        	if ($pos <= $u5l || $pos >= $u5l+$cdl) {next;}
+		my $indel=length($all1)-length($all2);
+		my $idl=abs($indel);
+		if ($indel !=0) {
+			my $deltype='insertion';
+			if ($indel >0 ) {$deltype='deletion';}
+			if ($indel%3 !=0) {
+				push(@vars,"$gene#$pos#frameshift $deltype of $idl bases");
+			}
+			else {
+				push(@vars,"$gene#$pos#codon(s) $deltype of $idl bases");
+			}
+			next;
+		}
+print "VAR $#vars $vars[$#vars]\n";
+        	my $base=substr($cds,$pos-$u5l-1,length($all1));
+        	my $env=substr($cds,$pos-$u5l-3,length($all1)+4);
+        	my $genv=substr($gnseq,$pos-3,length($all1)+4);
+        	if ($base ne $all1 && $base ne $all2) {print "$gene $pos $base ne $all1\n";next;}
+        	else {
+                	my $picodon=($pos-$u5l-1)%3;
+                	my $oricodon=substr($cds,($pos-$u5l-1)-$picodon,3);
+                	my $altcodon="";
+                	for (my $i=0;$i<3; $i++) {
+                        	if ($i==$picodon) {
+                                	$altcodon=$altcodon.$all2;
+                        	}
+                        	else {
+                                	$altcodon=$altcodon.substr($oricodon,$i,1);
+                        	}
+                	}
+                	my $protpos=int(($pos-$u5l)/3)+1;
+                	if ($codons_table{$altcodon} ne $codons_table{$oricodon}) {
+				if ($codons_table{$altcodon} eq '.') {
+					push(@vars,"$gene#$pos#".$codons_table{$oricodon}."$protpos -> STOP CODON ($oricodon / $altcodon)\n");
+				}
+				else {
+                			push(@vars,"$gene#$pos#".$codons_table{$oricodon}."$protpos".$codons_table{$altcodon}."($oricodon / $altcodon)\n");
+				}
+			}
+        	}
+	
+	}
+	return @vars;
+}
 1;
